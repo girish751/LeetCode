9d3d2ad83183a7bea1458d23ec9c0f4ec66dc28d
GIRISH CHAND SHARMA <girish316sharma@gmail.com>
2025-02-03 22:49:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        stack<char>str;
+     for(int i=0;i<s.length();i++){
+        char ch=s[i];
+        if(ch=='('||ch=='{'||ch=='[')
+        {
+            str.push(ch);
+        }
+        else{
+            if(!str.empty()){
+                char top=str.top();
+                if((ch==')'&& top=='(')||(ch=='}'&&top=='{')||(ch==']'&&top=='['))
+                {
+                    str.pop();
+                }
+            }
+            else{
+                return false;
+            }
+        }
+     }
+     if(str.empty()){
+        return true;
+     }
+     else{
+        return false;
+     }
+    }
+};
