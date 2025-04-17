b495f1237b7ee138a8bb6fe1e86892d07d67f4b8
GIRISH CHAND SHARMA <girish316sharma@gmail.com>
2025-04-17 21:45:37 +0530

    Time: 0 ms (100%), Space: 10.3 MB (82.98%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,37 +1,64 @@
-class Solution {
-public:
+// class Solution {
+// public:
 // string::npos ==string::npos is a special constant used in C++ to indicate "not found" when you're working with strings.
 
+//time complexity is O(n) but space is O(n)
 
-
-    string reverseVowels(string s) {
-        string vowels = "aeiouAEIOU";
-        vector<char> arr;
+//     string reverseVowels(string s) {
+//         string vowels = "aeiouAEIOU";
+//         vector<char> arr;
 
         
-        for (int i = 0; i < s.length(); i++) {
-            if (vowels.find(s[i]) != string::npos) {
-                arr.push_back(s[i]);
-            }
-        }
-        int i = 0, j = arr.size() - 1;
+//         for (int i = 0; i < s.length(); i++) {
+//             if (vowels.find(s[i]) != string::npos) {
+//                 arr.push_back(s[i]);
+//             }
+//         }
+//         int i = 0, j = arr.size() - 1;
+//         while (i < j) {
+//             swap(arr[i], arr[j]);
+//             i++;
+//             j--;
+//         }
+//         int k = 0;
+//         string ans = "";
+//         for (int i = 0; i < s.length(); i++) {
+//             if (vowels.find(s[i]) != string::npos) {
+//                 ans += arr[k];
+//                 k++;
+//             }
+//              else {
+//                 ans += s[i];
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+////time complexity is O(n) but space is O(1)
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string vowels = "aeiouAEIOU";
+        int i = 0, j = s.length() - 1;
+
         while (i < j) {
-            swap(arr[i], arr[j]);
+            while (i < j && vowels.find(s[i]) == string::npos){
+                  i++;
+            }
+
+            while (i < j && vowels.find(s[j]) == string::npos){
+                   j--;
+            }
+
+            swap(s[i], s[j]);
             i++;
             j--;
         }
-        int k = 0;
-        string ans = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (vowels.find(s[i]) != string::npos) {
-                ans += arr[k];
-                k++;
-            }
-             else {
-                ans += s[i];
-            }
-        }
 
-        return ans;
+        return s;
     }
 };
+
