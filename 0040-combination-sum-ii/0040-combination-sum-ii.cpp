b1bc1a51134fa61142c1f1e3c47773977bc6432a
GIRISH CHAND SHARMA <girish316sharma@gmail.com>
2025-05-24 17:44:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    set<vector<int>>s;
+   void  combination(vector<int>& candidates,int i,        vector<vector<int>>&ans,vector<int>temp,int target){
+        if(target==0){
+            sort(temp.begin(),temp.end());
+            if(s.find(temp)==s.end()){
+            ans.push_back(temp);
+            s.insert(temp);
+            }
+            return;
+        }
+        if(i==candidates.size() || target<0){
+            return;
+        }
+
+        //include single
+        temp.push_back(candidates[i]);
+        combination(candidates,i+1,ans,temp,target-candidates[i]);
+        //exclude
+        temp.pop_back();
+        combination(candidates,i+1,ans,temp,target);
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        combination(candidates,0,ans,temp,target);
+        return ans;
+        
+    }
+};
