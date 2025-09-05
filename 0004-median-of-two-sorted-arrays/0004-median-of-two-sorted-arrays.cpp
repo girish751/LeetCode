5a0ea096b0e752df532493933be1bd3b96a26fc1
GIRISH CHAND SHARMA <girish316sharma@gmail.com>
2025-09-05 15:27:00 +0530

    Time: 0 ms (100%), Space: 95.9 MB (19.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        
-   
-        if (n1 > n2)
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int n = n1 + n2;
-        int left = (n1 + n2 + 1) / 2; 
-        int low = 0, high = n1;
-        
-        while (low <= high) {
-            int mid1 = (low + high) >> 1; 
-            int mid2 = left - mid1; 
-            
-            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
-            
-        
-            if (mid1 < n1)
-                r1 = nums1[mid1];
-            if (mid2 < n2)
-                r2 = nums2[mid2];
-            if (mid1 - 1 >= 0)
-                l1 = nums1[mid1 - 1];
-            if (mid2 - 1 >= 0)
-                l2 = nums2[mid2 - 1];
-            
-            if (l1 <= r2 && l2 <= r1) {
-             
-                if (n % 2 == 1)
-                    return max(l1, l2);
-                else
-                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+        vector<float>ans;
+        int i=0;
+        int j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<=nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
             }
-            else if (l1 > r2) {
-               
-                high = mid1 - 1;
-            }
-            else {
-                
-                low = mid1 + 1;
+            else{
+                ans.push_back(nums2[j]);
+                j++;
             }
         }
-        
-        return 0; 
+        while(i<nums1.size()){
+            ans.push_back(nums1[i]);
+            i++;
+        }
+        while(j<nums2.size()){
+            ans.push_back(nums2[j]);
+            j++;
+        }
+        int n=ans.size();
+        int x=n/2;
+        if(n%2==0){
+            float y= (ans[x-1]+ans[x])/2;
+
+            return(y);
+
+        }
+            return ans[x];
+     
     }
 };
