4d2cdee347b1fd8a0be5c7c2509d2a0ac6b425e1
GIRISH CHAND SHARMA <girish316sharma@gmail.com>
2025-09-05 22:25:11 +0530

    Time: 13 ms (44.82%), Space: 35.1 MB (15.56%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,50 +1,39 @@
 class Solution {
     int sum(vector<int>&weights){
+        int i=0;
         int ans=0;
         for(int i=0;i<weights.size();i++){
             ans+=weights[i];
         }
         return ans;
-    }
-    int count(vector<int>weight,int days){
-        int day=1;
-        int load=0;
-        for(int i=0;i<weight.size();i++){
-            if(load+weight[i]>days){
-                day+=1;
-                load=weight[i];
-            }
-            else{
-                load+=weight[i];
-            }
-        }
-        return day;
-    }
-    int maxium(vector<int>&weights){    
-         int maxi=INT_MIN;    
-          for(int i=0;i<weights.size();i++){   
-                  maxi=max(maxi,weights[i]); 
-              }
-              return maxi;
-        }
-        
 
+    }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-     
-        int start=maxium(weights);
+        int start=*max_element(weights.begin(), weights.end());
         int end=sum(weights);
+      int x=end;
         while(start<=end){
+              int ans=0;
+              int y=1;
             int mid=start+(end-start)/2;
-            if(count(weights,mid)<=days){
+            for(int i=0;i<weights.size();i++){
+             
+                if(ans+weights[i]>mid){
+                    y++;
+                    ans=0;
+                }
+                ans+=weights[i];
+            }
+            if(y<=days){
+                x=mid;
                 end=mid-1;
-
             }
             else{
                 start=mid+1;
             }
         }
-        return start;
+        return x;
         
     }
 };
