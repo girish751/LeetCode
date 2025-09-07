3a7e60e4eb916d4b7422e791902021e980993b29
GIRISH CHAND SHARMA <girish316sharma@gmail.com>
2025-09-07 23:08:18 +0530

    Time: 0 ms (100%), Space: 21.6 MB (93.9%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>>ans;
+        vector<vector<int>> merged;
         int i = 0;
-        int n = intervals.size();
-        while(i < n && intervals[i][1] < newInterval[0]){
-          ans.push_back(intervals[i]);
-          i++;
+
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            merged.push_back(intervals[i]);
+            i++;
         }
-        while(i < n && intervals[i][0] <= newInterval[1]){
-          newInterval[0] = min(intervals[i][0],newInterval[0]);
-          newInterval[1] = max(intervals[i][1],newInterval[1]);
-          i++;
+
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval = {min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])};
+            i++;
         }
-        ans.push_back(newInterval);
-        while(i < n){
-          ans.push_back(intervals[i]);
-          i++;
+        merged.push_back(newInterval);
+
+        while (i < intervals.size()) {
+            merged.push_back(intervals[i]);
+            i++;
         }
-        return ans;
+
+        return merged;
     }
 };
