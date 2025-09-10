a86529db737c160954be692a84680999a34098f6
GIRISH CHAND SHARMA <girish316sharma@gmail.com>
2025-09-10 22:13:30 +0530

    Time: 146 ms (5.09%), Space: 14.2 MB (63.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>ans;
-        for(int i=0;i<nums.size()-1;i++){
+        for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
                     ans.push_back(i);
                     ans.push_back(j);
+                   
                 }
             }
         }
-        return ans;
-        
+        return ans;    
     }
 };
